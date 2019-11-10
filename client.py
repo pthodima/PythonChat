f29cd0e61b3959987e5fe9dc8fc246318b1a52bf
Cipher-2k <f20170189@hyderabad.bits-pilani.ac.in>
2019-11-10 17:31:46 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,15 +1,69 @@
 #!/usr/bin/env python3
-"""Server for multithreaded chat application."""
+""" GUI Client for chat application"""
 
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import tkinter
 
-clients = {}
-addresses = {}
 
-HOST = ''
-PORT = 42069
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:  # Can Happen when client has left the chat
+            break
+
+
+def send(event=None):  # even passed using binders.
+    msg = my_msg.get()
+    my_msg.set("")  # clear the input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        top.quit()
+
+
+def on_closing(event=None):
+    """This function is called when the window is closed."""
+    my_msg.set("{quit}")
+    send()
+
+
+top = tkinter.Tk()
+top.title("Cipher_Chat")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+my_msg.set("Type Your messages Here:")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To enable to see past messages.
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+HOST = input('Enter Host: ')
+PORT = input('Enter port: ')
+if not PORT:
+    PORT = 42069
+else:
+    PORT = int(PORT)
+
 BUFFER_SIZE = 1024
 ADDRESS = (HOST, PORT)
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDRESS)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDRESS)
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from threading import Thread
 clients = {}
 addresses = {}
 
-HOST = ''
+HOST = '127.0.0.1'
 PORT = 42069
 BUFFER_SIZE = 1024
 ADDRESS = (HOST, PORT)
@@ -15,47 +15,47 @@ SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDRESS)
 
 
-
 def accept_incoming_connections():
-	"""Sets up handling for incoming clients."""
-	while True:
-		client, client_address = SERVER.accept()
-		print("%s:%s has connected." %client_address)
-		client.send(bytes("Hello, from TheIntelligentImbecile" + "Now, type your name and press enter", "utf8"))
-		addreses[client] = client_address
-		Thread(target=handle_client, args=(client, )).start()
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Hello, from The IntelligentImbecile\n" + "Now, type your name and press enter", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client, )).start()
 
 
+def handle_client(client):  # This method takes client socket as an argument
+    """Handles a single client connection."""
+    name = client.recv(BUFFER_SIZE).decode("utf8")
+    welcome = "Welcome %s! If you ever want to quit, type {quit} to exit." % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(BUFFER_SIZE)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name + ": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
 
-def handle_client(client): #This method takes client socket as an argument
-	"""Handles a single client connection."""
-	name = client.recv(BUFFER_SIZE).decode("utf8")
-	welcome = "Welcome %s! If you ever want to quit, type {quit} to exit." % name
-	client.send(welcome, "utf8")
-	msg = "%s has joined the chat!" % name
-	broadcast(bytes(msg, "utf8"))
-	clients[client] = name
-		while True:
-			msg = client.recv(BUFFER_SIZE)
-			if msg != bytes("{quit}", "utf8")
-			client.close()
-			del clients[client]
-			broadcast(bytes("%s has left the chat." % name, "utf8"))
-			break
 
-
-
-def broadcast(msg, prefix=""): # The prefix is for appending userName:
-	"""Broadcasts a message to all clients."""
-	for sock in clients:
-		sock.send(bytes(prefix, "utf8")+msg)
-
+def broadcast(msg, prefix=""):  # The prefix is for appending userName:
+    """Broadcasts a message to all clients."""
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
 
 
 if __name__ == "__main__":
-	SERVER.listen(5) #Listens for % connections at max.
-	print("Waiting for connection....")
-	ACCEPT_THREAD = Thread(target = accept_incoming_connections)
-	ACCEPT_THREAD.start() #Starts the infinite loop in which the thread will run
-	ACCEPT_THREAD.join()
-	SERVER.CLOSE()
+    print(ADDRESS)
+    SERVER.listen(5)  # Listens for % connections at max.
+    print("Waiting for connection....")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()  # Starts the infinite loop in which the thread will run
+    ACCEPT_THREAD.join()
+    SERVER.close()
